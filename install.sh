9194b73f2243a02c406e984b69d303133e8eaff8
Matt Parlette <matthew.parlette@gmail.com>
2012-10-17 10:13:31 -0400

    added progress messages

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sudo apt-get -y install subversion
-sudo apt-get -y install mysql-server rabbitmq-server python-dev make swig autoconf
-sudo apt-get -y install memcached openjdk-7-jdk maven nagios-plugins-standard
-sudo apt-get -y install bc libmysqlclient-dev
-sudo apt-get -y install build-essential libreadline-dev libsnmp-dev libssl-dev zip unzip
-sudo apt-get -y install libaio1 libpython-lxml libpng3
+echo "installing prerequisite software..."
+sudo apt-get -y -qq install subversion
+sudo apt-get -y -qq install mysql-server rabbitmq-server python-dev make swig autoconf
+sudo apt-get -y -qq install memcached openjdk-7-jdk maven nagios-plugins-standard
+sudo apt-get -y -qq install bc libmysqlclient-dev
+sudo apt-get -y -qq install build-essential libreadline-dev libsnmp-dev libssl-dev zip unzip
+sudo apt-get -y -qq install libaio1 libpython-lxml libpng3
 
+echo "installing zlib..."
 #(trouble with zlib, install manually)
 #(http://www.techsww.com/tutorials/libraries/zlib/installation/installing_zlib_on_ubuntu_linux.php)
 wget http://www.zlib.net/zlib-1.2.7.tar.gz
@@ -15,15 +17,18 @@ cd zlib-1.2.7
 make
 sudo make install
 
+echo "adding zenoss user..."
 sudo useradd -m -U -s /bin/bash zenoss
 #(m:create home, U:create user group, s:shell)
 sudo mkdir /usr/local/zenoss
 sudo chown -R zenoss:zenoss /usr/local/zenoss
 
+echo "configuring rabbitmq..."
 sudo rabbitmqctl add_user zenoss zenoss
 sudo rabbitmqctl add_vhost /zenoss
 sudo rabbitmqctl set_permissions -p /zenoss zenoss '.*' '.*' '.*'
 
+echo "setting up zenoss user environment..."
 sudo su - zenoss
 #nano .bashrc
 echo 'export ZENHOME=/usr/local/zenoss' >> .bashrc
@@ -31,6 +36,7 @@ echo 'export PYTHONPATH=$ZENHOME/lib/python' >> .bashrc
 echo 'export PATH=$ZENHOME/bin:$PATH' >> .bashrc
 echo 'export INSTANCE_HOME=$ZENHOME' >> .bashrc
 
+echo "downloading zenoss install files..."
 svn co http://dev.zenoss.org/svn/tags/zenoss-4.2.0/inst zenoss-inst
 cd zenoss-inst
 
